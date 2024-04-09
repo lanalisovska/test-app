e77798b15164578d4016482da4b0784c067d89ef
Lana Lisovs'ka <76498128+lanalisovska@users.noreply.github.com>
2024-04-09 15:35:42 +0300

    update index
    add PersonList with new data props

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import React, { useState } from 'react';
 import styles from '@/styles/page.module.css';
 import { getPeopleList } from './api/getPeopleList';
@@ -28,13 +29,59 @@ const Home = ({ data }: any) => {
         ))}
       </div>
     </main>
+=======
+import React, { useEffect, useState, useRef } from 'react';
+import { getFilmsList, getPeopleList, getStarshipsList } from './api/apiRequests';
+import { PersonsList, MainPageWrapper } from '@/styles/MainPage.styled';
+import PersonItem from '@/components/PersonItem';
+import 'reactflow/dist/style.css';
+import { Person, type MainPageIterface } from '@/interfaces';
+
+const Home = ({ people, films, starships }: MainPageIterface) => {
+  const [peopleList, setPeopleList] = useState(people?.results);
+  const wrapperRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    setPeopleList(people?.results);
+  }, [people]);
+
+  const fetchNextPage = () => {
+    setPeopleList((prevData: Person[]) => [...prevData, ...peopleList]);
+  };
+
+  const handleScroll = () => {
+    const wrapper = wrapperRef.current;
+    if (wrapper !== null && wrapper !== undefined) {
+      const { scrollLeft, scrollWidth, clientWidth } = wrapper;
+      if (scrollLeft === scrollWidth - clientWidth) {
+        fetchNextPage();
+      }
+    }
+  };
+  return (
+    <MainPageWrapper>
+      <PersonsList ref={wrapperRef} onScroll={handleScroll}>
+        {peopleList?.map((person: Person) => (
+          <PersonItem key={person.id} person={person} films={films.results} starships={starships} />
+        ))}
+      </PersonsList>
+    </MainPageWrapper>
+>>>>>>> Stashed changes
   );
 };
 
 export async function getStaticProps() {
+<<<<<<< Updated upstream
   const data = await getPeopleList();
   return {
     props: { data },
+=======
+  const people = await getPeopleList();
+  const films = await getFilmsList();
+  const starships = await getStarshipsList();
+  return {
+    props: { people, films, starships },
+>>>>>>> Stashed changes
   };
 }
 export default Home;
